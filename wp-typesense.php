7213f021787fa9a2511d858ace82bc83e6cc818c
Rio Bahtiar <akhibahtiar@gmail.com>
2021-04-22 06:26:09 +0700

    add composer and plugin file #1

---
+++ wp-typesense.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: WordPress Typesense
+ * Plugin URI: https://rio.my.id/wp-typesense/
+ * Description: Supercharge WordPress Search with Typesense. Typesense is a fast, typo-tolerant search engine for building delightful search experiences.
+ * Version: 0.0.1
+ * Author: Rio Bahtiar
+ * Author URI: https://rio.my.id
+ * Text Domain: wp-typesense
+ * Domain Path: /languages/
+ * Requires at least: 5.5
+ * Requires PHP: 7.1
+ *
+ * @package wptypesense
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'WC_PLUGIN_FILE' ) ) {
+	define( 'WC_PLUGIN_FILE', __FILE__ );
+}
+
